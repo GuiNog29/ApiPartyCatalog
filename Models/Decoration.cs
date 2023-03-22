fa663ca83e3a38adafd3c8d9d8af5ec56cad24ae
Guilherme Almeida <guimathiasgui@gmail.com>
2023-03-22 19:50:39 -0300

    Adicionando models - 22/03

---
+++ Models/Decoration.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiPartyCatalog.Models
+{
+    public class Decoration
+    {
+        public int DecorationId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal ValueDecoration { get; set; }
+        public int DecoratorId { get; set; }
+        public Decorator Decorator { get; set; }
+    }
+}
---
+++ Models/Decorator.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiPartyCatalog.Models
+{
+    public class Decorator
+    {
+        public int DecoratorId { get; set; }
+        public string NameDecorator { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
