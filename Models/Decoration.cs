d16d385d12aef9d3cdb9a757586e0c6a6430ce7c
Guilherme Almeida <guimathiasgui@gmail.com>
2023-03-22 21:30:15 -0300

    Adicionando data annotations

--- Models/Decoration.cs
+++ Models/Decoration.cs
@@ -1,11 +1,22 @@
-﻿namespace ApiPartyCatalog.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPartyCatalog.Models
 {
     public class Decoration
     {
         public int DecorationId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+
+        [Required]
+        [MaxLength(5000)]
         public string Description { get; set; }
-        public decimal ValueDecoration { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+        public string UrlImage { get; set; }
         public int DecoratorId { get; set; }
         public Decorator Decorator { get; set; }
     }
